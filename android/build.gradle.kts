1853c467c848983cdb280fd431074d7b99b6ad45
doomsdayrs <doomsdayrs@gmail.com>
2023-01-15 13:12:41 -0500

    Upgrade appcompat to 1.6.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -223,7 +223,7 @@ dependencies {
 	// Androidx
 	implementation("androidx.work:work-runtime:2.7.1")
 	implementation("androidx.work:work-runtime-ktx:2.7.1")
-	implementation("androidx.appcompat:appcompat:1.5.1")
+	implementation("androidx.appcompat:appcompat:1.6.0")
 	implementation("androidx.annotation:annotation:1.5.0")
 	implementation("androidx.core:core-ktx:1.9.0")
 	implementation("androidx.collection:collection-ktx:1.2.0")
