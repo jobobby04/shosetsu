3871bd2cb7f72b8fafb01a7a55997da18e11d47e
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 13:36:41 -0500

    Fix library button having a background

--- android/src/main/java/app/shosetsu/android/view/compose/Button.kt
+++ android/src/main/java/app/shosetsu/android/view/compose/Button.kt
@@ -22,14 +22,11 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun TextButton(
@@ -37,12 +34,12 @@ fun TextButton(
 	modifier: Modifier = Modifier,
 	onLongClick: (() -> Unit)? = null,
 	enabled: Boolean = true,
-	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+	shape: Shape = ButtonDefaults.textShape,
+	colors: ButtonColors = ButtonDefaults2.textButtonColors(),
 	elevation: ButtonElevation? = null,
-	shape: Shape = MaterialTheme.shapes.small,
 	border: BorderStroke? = null,
-	colors: ButtonColors = ButtonDefaults.textButtonColors(),
 	contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
+	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 	content: @Composable RowScope.() -> Unit,
 ) =
 	Button(
@@ -69,31 +66,135 @@ fun Button(
 	elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
 	shape: Shape = MaterialTheme.shapes.small,
 	border: BorderStroke? = null,
-	colors: ButtonColors = ButtonDefaults.buttonColors(),
+	colors: ButtonColors = ButtonDefaults2.buttonColors(),
 	contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
 	content: @Composable RowScope.() -> Unit
 ) {
+	val containerColor = colors.containerColor(enabled).value
+	val contentColor = colors.contentColor(enabled).value
 	Surface(
 		onClick = onClick,
 		onLongClick = onLongClick,
+		color = containerColor,
+		contentColor = contentColor,
 		modifier = modifier,
 		enabled = enabled,
 		shape = shape,
 		border = border,
 		interactionSource = interactionSource,
 	) {
-		ProvideTextStyle(value = MaterialTheme.typography.labelLarge) {
-			Row(
-				Modifier
-					.defaultMinSize(
-						minWidth = ButtonDefaults.MinWidth,
-						minHeight = ButtonDefaults.MinHeight,
-					)
-					.padding(contentPadding),
-				horizontalArrangement = Arrangement.Center,
-				verticalAlignment = Alignment.CenterVertically,
-				content = content,
-			)
+		CompositionLocalProvider(LocalContentColor provides contentColor) {
+			ProvideTextStyle(value = MaterialTheme.typography.labelLarge) {
+				Row(
+					Modifier
+						.defaultMinSize(
+							minWidth = ButtonDefaults.MinWidth,
+							minHeight = ButtonDefaults.MinHeight,
+						)
+						.padding(contentPadding),
+					horizontalArrangement = Arrangement.Center,
+					verticalAlignment = Alignment.CenterVertically,
+					content = content,
+				)
+			}
 		}
 	}
+}
+
+@Immutable
+class ButtonColors internal constructor(
+	private val containerColor: Color,
+	private val contentColor: Color,
+	private val disabledContainerColor: Color,
+	private val disabledContentColor: Color,
+) {
+	/**
+	 * Represents the container color for this button, depending on [enabled].
+	 *
+	 * @param enabled whether the button is enabled
+	 */
+	@Composable
+	internal fun containerColor(enabled: Boolean): State<Color> {
+		return rememberUpdatedState(if (enabled) containerColor else disabledContainerColor)
+	}
+
+	/**
+	 * Represents the content color for this button, depending on [enabled].
+	 *
+	 * @param enabled whether the button is enabled
+	 */
+	@Composable
+	internal fun contentColor(enabled: Boolean): State<Color> {
+		return rememberUpdatedState(if (enabled) contentColor else disabledContentColor)
+	}
+
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other == null || other !is ButtonColors) return false
+
+		if (containerColor != other.containerColor) return false
+		if (contentColor != other.contentColor) return false
+		if (disabledContainerColor != other.disabledContainerColor) return false
+		if (disabledContentColor != other.disabledContentColor) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = containerColor.hashCode()
+		result = 31 * result + contentColor.hashCode()
+		result = 31 * result + disabledContainerColor.hashCode()
+		result = 31 * result + disabledContentColor.hashCode()
+		return result
+	}
+}
+
+object ButtonDefaults2 {
+	/**
+	 * Creates a [ButtonColors] that represents the default container and content colors used in a
+	 * [Button].
+	 *
+	 * @param containerColor the container color of this [Button] when enabled.
+	 * @param contentColor the content color of this [Button] when enabled.
+	 * @param disabledContainerColor the container color of this [Button] when not enabled.
+	 * @param disabledContentColor the content color of this [Button] when not enabled.
+	 */
+	@Composable
+	fun buttonColors(
+		containerColor: Color = MaterialTheme.colorScheme.primary,
+		contentColor: Color = MaterialTheme.colorScheme.onPrimary,
+		disabledContainerColor: Color =
+			MaterialTheme.colorScheme.onSurface
+				.copy(alpha = 0.12f),
+		disabledContentColor: Color = MaterialTheme.colorScheme.onSurface
+			.copy(alpha = 0.38f),
+	): ButtonColors = ButtonColors(
+		containerColor = containerColor,
+		contentColor = contentColor,
+		disabledContainerColor = disabledContainerColor,
+		disabledContentColor = disabledContentColor
+	)
+
+	/**
+	 * Creates a [ButtonColors] that represents the default container and content colors used in a
+	 * [TextButton].
+	 *
+	 * @param containerColor the container color of this [TextButton] when enabled
+	 * @param contentColor the content color of this [TextButton] when enabled
+	 * @param disabledContainerColor the container color of this [TextButton] when not enabled
+	 * @param disabledContentColor the content color of this [TextButton] when not enabled
+	 */
+	@Composable
+	fun textButtonColors(
+		containerColor: Color = Color.Transparent,
+		contentColor: Color = MaterialTheme.colorScheme.primary,
+		disabledContainerColor: Color = Color.Transparent,
+		disabledContentColor: Color = MaterialTheme.colorScheme.onSurface
+			.copy(alpha = 0.38f),
+	): ButtonColors = ButtonColors(
+		containerColor = containerColor,
+		contentColor = contentColor,
+		disabledContainerColor = disabledContainerColor,
+		disabledContentColor = disabledContentColor
+	)
 }
