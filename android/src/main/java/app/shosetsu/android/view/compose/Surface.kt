6c9b75b8d9436a897e6abbc937f2da46620dca50
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 14:24:26 -0500

    Material 3 surface

--- android/src/main/java/app/shosetsu/android/view/compose/Surface.kt
+++ android/src/main/java/app/shosetsu/android/view/compose/Surface.kt
@@ -21,7 +21,8 @@ package app.shosetsu.android.view.compose
 import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.NonRestartableComposable
@@ -44,21 +45,19 @@ fun Surface(
 	modifier: Modifier = Modifier,
 	onLongClick: (() -> Unit)? = null,
 	shape: Shape = RectangleShape,
-	color: Color = MaterialTheme.colors.surface,
+	color: Color = MaterialTheme.colorScheme.surface,
 	contentColor: Color = contentColorFor(color),
 	border: BorderStroke? = null,
-	elevation: Dp = 0.dp,
+	tonalElevation: Dp = 0.dp,
+	shadowElevation: Dp = 0.dp,
 	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-	indication: Indication? = LocalIndication.current,
 	enabled: Boolean = true,
-	onClickLabel: String? = null,
-	role: Role? = null,
 	content: @Composable () -> Unit,
 ) {
-	val absoluteElevation = LocalAbsoluteElevation.current + elevation
+	val absoluteElevation = LocalAbsoluteTonalElevation.current + tonalElevation
 	CompositionLocalProvider(
 		LocalContentColor provides contentColor,
-		LocalAbsoluteElevation provides absoluteElevation
+		LocalAbsoluteTonalElevation provides absoluteElevation
 	) {
 		Box(
 			modifier
@@ -67,22 +66,18 @@ fun Surface(
 					shape = shape,
 					backgroundColor = surfaceColorAtElevation(
 						color = color,
-						elevationOverlay = LocalElevationOverlay.current,
-						absoluteElevation = absoluteElevation
+						elevation = absoluteElevation
 					),
 					border = border,
-					elevation = elevation
+					shadowElevation = shadowElevation
 				)
-				.then(
-					Modifier.combinedClickable(
-						interactionSource = interactionSource,
-						indication = indication,
-						enabled = enabled,
-						onClickLabel = onClickLabel,
-						role = role,
-						onClick = onClick,
-						onLongClick = onLongClick
-					)
+				.combinedClickable(
+					interactionSource = interactionSource,
+					indication = rememberRipple(),
+					enabled = enabled,
+					role = Role.Button,
+					onClick = onClick,
+					onLongClick = onLongClick
 				),
 			propagateMinConstraints = true
 		) {
@@ -95,21 +90,16 @@ private fun Modifier.surface(
 	shape: Shape,
 	backgroundColor: Color,
 	border: BorderStroke?,
-	elevation: Dp
-) = this
-	.shadow(elevation, shape, clip = false)
+	shadowElevation: Dp
+) = this.shadow(shadowElevation, shape, clip = false)
 	.then(if (border != null) Modifier.border(border, shape) else Modifier)
 	.background(color = backgroundColor, shape = shape)
 	.clip(shape)
 
 @Composable
-private fun surfaceColorAtElevation(
-	color: Color,
-	elevationOverlay: ElevationOverlay?,
-	absoluteElevation: Dp
-): Color {
-	return if (color == MaterialTheme.colors.surface && elevationOverlay != null) {
-		elevationOverlay.apply(color, absoluteElevation)
+private fun surfaceColorAtElevation(color: Color, elevation: Dp): Color {
+	return if (color == MaterialTheme.colorScheme.surface) {
+		MaterialTheme.colorScheme.surfaceColorAtElevation(elevation)
 	} else {
 		color
 	}
