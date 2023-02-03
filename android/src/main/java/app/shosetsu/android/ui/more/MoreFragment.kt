0818c92292a9e816b16e5f403997533507612c01
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 14:33:51 -0500

    Remove useless styles

--- android/src/main/java/app/shosetsu/android/ui/more/MoreFragment.kt
+++ android/src/main/java/app/shosetsu/android/ui/more/MoreFragment.kt
@@ -237,9 +237,9 @@ fun MoreContent(
 				)
 			}
 
-			item {
+			/*item {
 				MoreItemContent(R.string.styles, R.drawable.ic_baseline_style_24, showStyleBar)
-			}
+			}*/
 
 			item {
 				MoreItemContent(
