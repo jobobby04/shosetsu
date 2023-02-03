aded8c1e9a0b64bbe96ba547f680e697a0e4bb2b
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 12:02:47 -0500

    Disable site protector

--- android/src/main/java/app/shosetsu/android/providers/network/NetworkControls.kt
+++ android/src/main/java/app/shosetsu/android/providers/network/NetworkControls.kt
@@ -3,8 +3,6 @@ package app.shosetsu.android.providers.network
 import app.shosetsu.android.common.consts.USER_AGENT
 import app.shosetsu.android.common.ext.logI
 import app.shosetsu.android.common.utils.CookieJarSync
-import app.shosetsu.android.common.utils.SiteProtector
-import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -36,13 +34,8 @@ fun createOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
 	.cookieJar(CookieJarSync)
 	.addInterceptor {
 		val r = it.request().newBuilder().header("User-Agent", USER_AGENT).build()
-		val response = runBlocking {
-			// Await for chance to access the site
-			SiteProtector.await(r.url.host) {
-				it.logI(r.toString())
-				it.proceed(r)
-			}
-		}
+		it.logI(r.toString())
+		val response = it.proceed(r)
 		it.logI(response.toString())
 		return@addInterceptor response
 	}.apply {
