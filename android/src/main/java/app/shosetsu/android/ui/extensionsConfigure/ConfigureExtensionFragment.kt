a6bb276312f54862e8c8ce03228bf07d34fd753f
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 23:21:06 -0500

    Improve missing icon in configure extension

--- android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtensionFragment.kt
+++ android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtensionFragment.kt
@@ -17,6 +17,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -358,7 +360,12 @@ fun ConfigureExtensionHeaderContent(
 						}
 					)
 				} else {
-					ImageLoadingError(Modifier.size(100.dp))
+					Box(Modifier.size(100.dp), contentAlignment = Alignment.Center) {
+						ImageLoadingError(
+							Modifier.size(80.dp)
+								.clip(MaterialTheme.shapes.extraSmall)
+						)
+					}
 				}
 
 				Column {
