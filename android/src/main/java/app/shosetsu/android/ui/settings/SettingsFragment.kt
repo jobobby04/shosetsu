7fb36ab2f335fd2f08843708983124b2a6b7510d
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 23:16:26 -0500

    Use regular box for SettingsMenuItem

--- android/src/main/java/app/shosetsu/android/ui/settings/SettingsFragment.kt
+++ android/src/main/java/app/shosetsu/android/ui/settings/SettingsFragment.kt
@@ -9,12 +9,9 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.material3.CardDefaults.cardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -80,18 +77,12 @@ fun SettingsView(
 	}
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingMenuItem(@StringRes title: Int, @DrawableRes drawableRes: Int, onClick: () -> Unit) {
-	Card(
+	Box(
 		modifier = Modifier
 			.clickable(onClick = onClick)
 			.fillMaxWidth(),
-		elevation = cardElevation(0.dp),
-		colors = CardDefaults.cardColors(
-			containerColor = colorResource(android.R.color.transparent),
-		),
-		shape = RectangleShape
 	) {
 		Row(
 			verticalAlignment = Alignment.CenterVertically
