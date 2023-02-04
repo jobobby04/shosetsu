6271dacae6d519c9d3efa0d47f31f75135e53868
Jobobby04 <jobobby04@users.noreply.github.com>
2023-02-03 23:18:37 -0500

    Use Material 3 chips for genre

--- android/src/main/java/app/shosetsu/android/ui/novel/NovelFragment.kt
+++ android/src/main/java/app/shosetsu/android/ui/novel/NovelFragment.kt
@@ -1560,17 +1560,15 @@ fun ExpandedText(
 	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun NovelGenre(
 	text: String
 ) {
-	Card {
-		Text(
-			text,
-			modifier = Modifier.padding(8.dp),
-			style = MaterialTheme.typography.bodyMedium
-		)
-	}
+	ElevatedSuggestionChip(
+		onClick = {},
+		label = { Text(text) },
+	)
 }
 
 @Composable
