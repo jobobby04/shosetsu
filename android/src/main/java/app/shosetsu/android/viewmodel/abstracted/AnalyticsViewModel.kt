8c489965b597d51ce581d05b380ec1abf1535895
doomsdayrs <doomsdayrs@gmail.com>
2023-04-15 21:05:04 -0400

    Properly document AnalyticsViewModel

--- android/src/main/java/app/shosetsu/android/viewmodel/abstracted/AnalyticsViewModel.kt
+++ android/src/main/java/app/shosetsu/android/viewmodel/abstracted/AnalyticsViewModel.kt
@@ -28,22 +28,73 @@ import kotlinx.coroutines.flow.Flow
  * @author Doomsdayrs
  */
 abstract class AnalyticsViewModel : ShosetsuViewModel() {
+	/**
+	 * Days that the user has spent reading
+	 */
 	abstract val days: Flow<Int>
+
+	/**
+	 * Hours that the user has spent reading minus [days]
+	 */
 	abstract val hours: Flow<Int>
+
+	/**
+	 * Minutes that the user has spend reading minus [hours]
+	 */
 	abstract val minutes: Flow<Int>
 
+	/**
+	 * Total count of novels in the users library
+	 */
 	abstract val totalLibraryNovelCount: Flow<Int>
+
+	/**
+	 * Total count of unread novels in the users library
+	 */
 	abstract val totalUnreadNovelCount: Flow<Int>
+
+	/**
+	 * Total count of novels currently being read in the users library
+	 */
 	abstract val totalReadingNovelCount: Flow<Int>
+
+	/**
+	 * Total count of read novels in the users library
+	 */
 	abstract val totalReadNovelCount: Flow<Int>
 
+	/**
+	 * Total count of chapters in the users library
+	 */
 	abstract val totalChapterCount: Flow<Int>
+
+	/**
+	 * Total count of unread chapters in the users library
+	 */
 	abstract val totalUnreadChapterCount: Flow<Int>
+
+	/**
+	 * Total count of chapters currently being read in the users library
+	 */
 	abstract val totalReadingChapterCount: Flow<Int>
+
+	/**
+	 * Total count of read chapters in the users library
+	 */
 	abstract val totalReadChapterCount: Flow<Int>
 
+	/**
+	 * Top 3 genres in the users library
+	 */
 	abstract val topGenres: Flow<List<String>>
+
+	/**
+	 * Top 3 extensions used in the users library
+	 */
 	abstract val topExtensions: Flow<List<String>>
 
+	/**
+	 * A collection of novels and their statistics from the users library.
+	 */
 	abstract val novels: Flow<List<AnalyticsNovelUI>>
 }
