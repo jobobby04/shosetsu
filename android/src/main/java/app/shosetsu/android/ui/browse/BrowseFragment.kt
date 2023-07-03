5f8751093d0734add775c6268a3bb0bc0f6263e3
Jobobby04 <jobobby04@users.noreply.github.com>
2023-07-02 21:29:20 -0400

    Import cleanup

--- android/src/main/java/app/shosetsu/android/ui/browse/BrowseFragment.kt
+++ android/src/main/java/app/shosetsu/android/ui/browse/BrowseFragment.kt
@@ -30,7 +30,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
