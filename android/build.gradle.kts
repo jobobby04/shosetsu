d6de433f73840610eeb4e39124df3eedb8e9a1a6
doomsdayrs <doomsdayrs@gmail.com>
2023-08-11 14:21:39 -0400

    Enable core library desugaring

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -174,6 +174,7 @@ android {
 	compileOptions {
 		sourceCompatibility = JavaVersion.VERSION_17
 		targetCompatibility = JavaVersion.VERSION_17
+		isCoreLibraryDesugaringEnabled = true
 	}
 	kotlinOptions {
 		jvmTarget = JavaVersion.VERSION_17.toString()
@@ -408,4 +409,5 @@ dependencies {
 	implementation(navigation("fragment-ktx"))
 	implementation(navigation("ui-ktx"))
 
+	coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
