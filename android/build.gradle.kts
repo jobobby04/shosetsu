061a1cc96540f9c30fc9256d0b4b7414d3bedcdc
doomsdayrs <doomsdayrs@gmail.com>
2023-09-16 11:26:15 -0400

    Update androidx.annotation to 1.7.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -225,7 +225,7 @@ dependencies {
 	implementation("androidx.work:work-runtime:2.8.1")
 	implementation("androidx.work:work-runtime-ktx:2.8.1")
 	implementation("androidx.appcompat:appcompat:1.6.1")
-	implementation("androidx.annotation:annotation:1.6.0")
+	implementation("androidx.annotation:annotation:1.7.0")
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.collection:collection-ktx:1.2.0")
 	implementation("androidx.core:core-splashscreen:1.0.1")
