ae36230ade927e368a1e7c95aea6be068c11b016
doomsdayrs <doomsdayrs@gmail.com>
2023-12-22 12:23:33 -0500

    Properly hide navigation bar on non-root views

--- android/src/main/java/app/shosetsu/android/ui/main/MainView.kt
+++ android/src/main/java/app/shosetsu/android/ui/main/MainView.kt
@@ -2,6 +2,9 @@ package app.shosetsu.android.ui.main
 
 import android.app.Activity
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material3.Icon
@@ -11,14 +14,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -104,7 +110,6 @@ import app.shosetsu.android.ui.updates.UpdatesView
 fun MainView() {
 	val context = LocalContext.current
 	val navController = rememberNavController()
-	val currentDestination = navController.currentDestination
 	val navBackStackEntry by navController.currentBackStackEntryAsState()
 
 	val destinations = listOf(
@@ -119,7 +124,7 @@ fun MainView() {
 	ShosetsuTheme {
 		Scaffold(
 			bottomBar = {
-				BottomNavigationBar(destinations, currentDestination, navController)
+				BottomNavigationBar(destinations, navBackStackEntry, navController)
 			},
 		) { paddingValues ->
 			NavHost(
@@ -179,43 +184,60 @@ fun MainView() {
 @Composable
 fun <T> BottomNavigationBar(
 	destinations: List<T>,
-	currentDestination: NavDestination?,
+	currentDestination: NavBackStackEntry?,
 	navController: NavController
 ) where T : Destination, T : Root {
-	NavigationBar {
-		destinations.forEach { destination ->
-			NavigationBarItem(
-				selected =
-				currentDestination?.hierarchy?.any {
-					it.route == destination.route
-				} == true,
-				icon = {
-					Icon(
-						painterResource(
-							destination.icon
-						),
-						destination.route
-					)
-				},
-				label = {
-					Text(destination.route)
-				},
-				onClick = {
-					navController.navigate(destination.route) {
-						// Pop up to the start destination of the graph to
-						// avoid building up a large stack of destinations
-						// on the back stack as users select items
-						popUpTo(navController.graph.findStartDestination().id) {
-							saveState = true
+	var isVisible by remember { mutableStateOf(true) }
+
+	LaunchedEffect(currentDestination) {
+		isVisible = destinations.any { destination ->
+			currentDestination?.destination?.route == destination.route ||
+					currentDestination?.destination?.route == "main"
+		}
+	}
+
+	AnimatedVisibility(
+		isVisible,
+		enter = slideInVertically {
+			it * 2
+		},
+		exit = slideOutVertically {
+			it * 2
+		}
+	) {
+		NavigationBar {
+			destinations.forEach { destination ->
+				NavigationBarItem(
+					selected =
+					currentDestination?.destination?.route == destination.route,
+					icon = {
+						Icon(
+							painterResource(
+								destination.icon
+							),
+							destination.route
+						)
+					},
+					label = {
+						Text(destination.route)
+					},
+					onClick = {
+						navController.navigate(destination.route) {
+							// Pop up to the start destination of the graph to
+							// avoid building up a large stack of destinations
+							// on the back stack as users select items
+							popUpTo(navController.graph.findStartDestination().id) {
+								saveState = true
+							}
+							// Avoid multiple copies of the same destination when
+							// reselecting the same item
+							launchSingleTop = true
+							// Restore state when reselecting a previously selected item
+							restoreState = true
 						}
-						// Avoid multiple copies of the same destination when
-						// reselecting the same item
-						launchSingleTop = true
-						// Restore state when reselecting a previously selected item
-						restoreState = true
 					}
-				}
-			)
+				)
+			}
 		}
 	}
 }
