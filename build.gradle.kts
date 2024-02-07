52342af2a618748e71787942c319fffe76050ad7
doomsdayrs <doomsdayrs@gmail.com>
2024-02-07 11:17:34 -0500

    Upgrade AGP to 8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("com.android.tools.build:gradle:8.2.0")
+		classpath("com.android.tools.build:gradle:8.2.2")
 		classpath(kotlin("gradle-plugin", version = kotlinVersion))
 		classpath(kotlin("serialization", version = kotlinVersion))
 	}
