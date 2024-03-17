32dc80233f92b3d8f6b10215d1b8afd726fcb65d
doomsdayrs <doomsdayrs@gmail.com>
2024-03-16 23:01:02 -0400

    Upgrade lifecycle-runtime-ktx to 2.7.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -241,7 +241,7 @@ dependencies {
 	implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 	implementation("androidx.window:window:1.2.0")
 	implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
 
 	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
