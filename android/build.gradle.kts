5fdffbe44b93a9faa88371c21589e877ad068268
doomsdayrs <doomsdayrs@gmail.com>
2024-03-16 23:02:59 -0400

    Upgrade android bom to 2024.02.02

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -244,11 +244,11 @@ dependencies {
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
 
-	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
 
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
 
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -366,7 +366,7 @@ dependencies {
 		version: String = androidxCompose
 	) = "androidx.compose.$submodule:$module:$version"
 
-	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.02"))
 	implementation("androidx.compose.runtime:runtime")
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-tooling")
