14a217bd0617e0c155d1a5d7dea608600c435b5f
doomsdayrs <doomsdayrs@gmail.com>
2024-03-16 23:04:12 -0400

    Upgrade kotlinx-coroutines-android to 1.8.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -292,7 +292,7 @@ dependencies {
 	implementation(kotlin("stdlib-jdk8"))
 	//implementation(kotlin("reflect"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
 	implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
 
