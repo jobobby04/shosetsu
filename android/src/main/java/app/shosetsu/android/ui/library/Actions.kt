2eb918297cd318781725c0bed94d2522dff37140
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-20 15:44:45 -0400

    Use Material3 in more places

--- android/src/main/java/app/shosetsu/android/ui/library/Actions.kt
+++ android/src/main/java/app/shosetsu/android/ui/library/Actions.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.RadioButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
