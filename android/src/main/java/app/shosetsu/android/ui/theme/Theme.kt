553a4f2e9dabd625e9c62c9a6855a697aa4cea7b
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-20 15:49:55 -0400

    Fix Shosetsu theme

--- android/src/main/java/app/shosetsu/android/ui/theme/Theme.kt
+++ android/src/main/java/app/shosetsu/android/ui/theme/Theme.kt
@@ -42,13 +42,6 @@ val FallbackColorScheme = LightColorScheme
 
 @Composable
 fun ShosetsuTheme(
-	content: @Composable () -> Unit
-) {
-	content()
-}
-
-@Composable
-fun ShosetsuTheme(
 	theme: AppThemes,
 	content: @Composable () -> Unit
 ) {
