599bd8872f9c55690e296e3ad08fc9b827cf390a
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-20 20:17:03 -0400

    Fix material3 in a place

--- android/src/main/java/app/shosetsu/android/ui/reader/page/HTMLPage.kt
+++ android/src/main/java/app/shosetsu/android/ui/reader/page/HTMLPage.kt
@@ -10,11 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalUriHandler
@@ -99,7 +106,7 @@ fun HTMLPage(
 			}
 		}
 
-	val backgroundColor = MaterialTheme.colors.background
+	val backgroundColor = MaterialTheme.colorScheme.background
 	ScrollStateBar(scrollState) {
 		WebView(
 			state = state,
