19898f5da45582b4c5f9bd56d53dcb7ae2ee4004
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-21 20:44:49 -0400

    Improve Status bar and Navigation bar coloring

--- android/src/main/java/app/shosetsu/android/ui/theme/Theme.kt
+++ android/src/main/java/app/shosetsu/android/ui/theme/Theme.kt
@@ -8,11 +8,13 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 
 
@@ -56,8 +58,10 @@ fun ShosetsuTheme(
 	if (!view.isInEditMode) {
 		SideEffect {
 			val window = (view.context as Activity).window
-			window.statusBarColor = colorScheme.primary.toArgb()
-			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+			val color = colorScheme.surfaceColorAtElevation(3.dp).toArgb()
+			window.statusBarColor = color
+			window.navigationBarColor = color
+			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
 		}
 	}
 
