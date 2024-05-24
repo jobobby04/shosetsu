67189a16af2a5466198b02b1c57c7f7b5df86cb3
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-24 10:43:11 -0400

    Set WebView background

--- android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
+++ android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
@@ -19,10 +19,28 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -35,7 +53,11 @@ import app.shosetsu.android.common.ext.toast
 import app.shosetsu.android.common.ext.viewModelDi
 import app.shosetsu.android.ui.theme.ShosetsuTheme
 import app.shosetsu.android.viewmodel.abstracted.WebViewViewModel
-import com.google.accompanist.web.*
+import com.google.accompanist.web.AccompanistWebViewClient
+import com.google.accompanist.web.LoadingState
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewNavigator
+import com.google.accompanist.web.rememberWebViewState
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.android.closestDI
@@ -279,6 +301,7 @@ fun WebViewScreen(
 			}
 		}
 
+		val background = MaterialTheme.colorScheme.background
 		WebView(
 			state = state,
 			modifier = Modifier
@@ -299,6 +322,8 @@ fun WebViewScreen(
 				) {
 					WebView.setWebContentsDebuggingEnabled(true)
 				}
+
+				webView.setBackgroundColor(background.toArgb())
 			},
 			client = webClient,
 			chromeClient = ShosetsuAccompanistWebChromeClient()
