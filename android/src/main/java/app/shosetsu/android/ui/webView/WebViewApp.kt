9cb3cfdb5bcbff9c7e7c5a7a8d84daf99e184347
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-24 10:43:11 -0400

    Set WebView background

--- android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
+++ android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
@@ -22,11 +22,16 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -35,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -288,6 +294,7 @@ fun WebViewScreen(
 			}
 		}
 
+		val background = MaterialTheme.colorScheme.background
 		WebView(
 			state = state,
 			modifier = Modifier
@@ -308,6 +315,8 @@ fun WebViewScreen(
 				) {
 					WebView.setWebContentsDebuggingEnabled(true)
 				}
+
+				webView.setBackgroundColor(background.toArgb())
 			},
 			client = webClient,
 			chromeClient = ShosetsuAccompanistWebChromeClient()
