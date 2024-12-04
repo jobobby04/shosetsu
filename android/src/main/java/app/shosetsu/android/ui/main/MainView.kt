d2464141c6b30077ef73603faddf41394d3e24a5
clocks <doomsdayrs@gmail.com>
2024-12-03 20:08:55 -0500

    Don't use material components

--- android/src/main/java/app/shosetsu/android/ui/main/MainView.kt
+++ android/src/main/java/app/shosetsu/android/ui/main/MainView.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.IconButton
+import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
