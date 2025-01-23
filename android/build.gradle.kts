b21806482847f9b2a8ce1ee5f3b40d105d691bc4
clocks <doomsdayrs@gmail.com>
2025-01-23 11:12:23 -0500

    Version Catalog: Move androidx lifecycle runtime ktx

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -242,7 +242,7 @@ dependencies {
 	implementation(libs.androidx.coordinatorlayout)
 	implementation(libs.androidx.window)
 	implementation(libs.androidx.compose.material3.wsc)
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+	implementation(libs.androidx.lifecycle.runtime.ktx)
 	implementation("androidx.activity:activity-compose:1.8.2")
 
 	implementation(platform("androidx.compose:compose-bom:2024.02.02"))
