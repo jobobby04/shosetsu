085ef938a96d5c9cf820cb2d22c9fdebe512c335
clocks <doomsdayrs@gmail.com>
2025-01-23 12:06:31 -0500

    Version Catalog: move agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("com.android.tools.build:gradle:8.8.0")
+		classpath(libs.gradle)
 		classpath(kotlin("gradle-plugin", version = kotlinVersion))
 		classpath(kotlin("serialization", version = kotlinVersion))
 	}
