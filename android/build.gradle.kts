69c4f7c5915a0754ea33cbd4aceb7da90244fc6c
clocks <doomsdayrs@gmail.com>
2025-01-23 11:12:01 -0500

    Version Catalog: Move androidx compose m3 window size class

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -241,7 +241,7 @@ dependencies {
 	implementation(libs.androidx.core.splashscreen)
 	implementation(libs.androidx.coordinatorlayout)
 	implementation(libs.androidx.window)
-	implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+	implementation(libs.androidx.compose.material3.wsc)
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
 
