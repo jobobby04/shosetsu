2e132ca6538096e33245699e1b3a489018d3fd6a
JFronny <git@akbvopenfl1.hopto.org>
2025-05-21 11:13:19 +0200

    build: make debug builds inspectable

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -109,8 +109,8 @@ android {
 			versionNameSuffix = "-${getCommitCount()}"
 			applicationIdSuffix = ".debug"
 			isDebuggable = true
-			isMinifyEnabled = true
-			isShrinkResources = true
+			isMinifyEnabled = false
+			isShrinkResources = false
 			proguardFiles(
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro"
