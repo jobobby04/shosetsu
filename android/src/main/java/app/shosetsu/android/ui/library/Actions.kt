0ccbb2db78754c61c3a0c350c659612e8221911f
Jobobby04 <jobobby04@users.noreply.github.com>
2025-07-01 15:32:42 -0400

    Fix enter key for search

--- android/src/main/java/app/shosetsu/android/ui/library/Actions.kt
+++ android/src/main/java/app/shosetsu/android/ui/library/Actions.kt
@@ -37,6 +37,11 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -249,7 +254,16 @@ fun SearchAction(
 				},
 				modifier = Modifier
 					.fillMaxWidth()
-					.focusRequester(focusRequester),
+					.focusRequester(focusRequester)
+					.onKeyEvent(
+						onKeyEvent = {
+							if (it.key == Key.Enter && it.type == KeyEventType.KeyUp) {
+								onSearch(searchQuery)
+								focusManager.clearFocus()
+							}
+							false
+						}
+					),
 				keyboardActions = KeyboardActions {
 					onSearch(searchQuery)
 					focusManager.clearFocus()
