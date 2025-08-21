6c29490035f12da052df523beb3e3c35344ed9e2
JFronny <git@akbvopenfl1.hopto.org>
2025-08-21 11:11:40 +0200

    feat: consistently use material 3 colors and spacing for number pickers

--- android/src/main/java/app/shosetsu/android/view/compose/StandardDialog.kt
+++ android/src/main/java/app/shosetsu/android/view/compose/StandardDialog.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -15,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -64,13 +68,22 @@ fun StandardDialog(
 	content: @Composable () -> Unit,
 ) {
 	Dialog(onDismissRequest, dialogProperties) {
-		Card {
+		Card(
+			modifier = Modifier.fillMaxWidth()
+				.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+		) {
 			Column(
-				Modifier.padding(16.dp)
+				modifier = Modifier.fillMaxWidth()
+					.padding(16.dp),
+				horizontalAlignment = Alignment.CenterHorizontally,
 			) {
-				ProvideTextStyle(MaterialTheme.typography.titleLarge) {
+				ProvideTextStyle(MaterialTheme.typography.titleLarge
+					.copy(textAlign = TextAlign.Center)
+				) {
 					title()
 				}
+
+				Spacer(Modifier.height(8.dp))
 
 				content()
 
