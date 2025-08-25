1e2bf13efe4591929c26bb725f1a10f71009d738
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-08-25 12:18:40 +0200

    fix(share link): query parameters not passed when opening from intent

--- android/src/main/java/app/shosetsu/android/ui/main/IntentHandler.kt
+++ android/src/main/java/app/shosetsu/android/ui/main/IntentHandler.kt
@@ -48,7 +48,7 @@ private fun handleIntentAction(
 				if (intent.data!!.scheme != null) {
 					onNavigate(
 						Destination.More.AddShare(
-							intent.data!!.scheme + "://" + intent.data!!.host
+							intent.data.toString()
 						)
 					)
 				} else intent.logE("Scheme was null")
