2867fa21e4e72b49999cff9c828c5594ca669fb1
JFronny <git@akbvopenfl1.hopto.org>
2026-03-09 13:38:31 +0100

    fix: do not use WebView in utility thread
    It does not have a Looper

--- android/src/main/java/app/shosetsu/android/di/UseCaseModule.kt
+++ android/src/main/java/app/shosetsu/android/di/UseCaseModule.kt
@@ -107,7 +107,7 @@ import org.kodein.di.provider
  * 01 / 05 / 2020
  */
 val useCaseModule: DI.Module = DI.Module("useCase") {
-	bind<GetUserAgentUseCase>() with provider { GetUserAgentUseCase(instance(), instance()) }
+	bind<GetUserAgentUseCase>() with provider { GetUserAgentUseCase(instance()) }
 	bind<LoadDownloadsUseCase>() with provider { LoadDownloadsUseCase(instance()) }
 
 	bind<LoadLibraryUseCase>() with provider { LoadLibraryUseCase(instance(), instance()) }
